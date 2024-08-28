63e3d87d655c083da34806e1529d6a080c20bfcb
pyPrarthan <christianprarthan07@gmail.com>
2024-08-28 14:39:06 -0400

    first simple chatbot

---
+++ echoChatBot.py
@@ -0,0 +1,15 @@
+'''
+You can build a small chatbot! In this project, you’ll apply your knowledge of:
+-> variable
+-> string concatenation
+-> output
+-> user input
+to create “Echo Chatbot,” a chatbot that echoes whatever a user inputs.
+'''
+
+print("Welcome to Prarthan's first chatbot!")
+print("I will repeat after you.")
+
+stuff_to_echo = input("What would you like to repeat: ")
+
+print('You said: ' + stuff_to_echo)
