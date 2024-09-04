f4ba37ef2993cd08037e23f8b115abcae8f48509
pyPrarthan <christianprarthan07@gmail.com>
2024-09-04 19:36:29 -0400

    small project finished

--- Discover Insights into Classic Texts/script.py
+++ Discover Insights into Classic Texts/script.py
@@ -31,7 +31,7 @@ print(single_pos_sentence)
 
 
 # define noun phrase chunk grammar here
-np_chunk_grammar "NP: {<DT>?<JJ>*<NN>}"
+np_chunk_grammar = "NP: {<DT>?<JJ>*<NN>}"
 
 # create noun phrase RegexpParser object here
 np_chunk_parser = RegexpParser(np_chunk_grammar)
@@ -53,8 +53,9 @@ for txt in pos_tagged_text:
   vp_chunked_text.append(vp_chunk_parser.parse(txt))
 
 # store and print the most common NP-chunks here
-
-
+most_common_np_chunks = np_chunk_counter(np_chunked_text )
+print(most_common_np_chunks)
 
 # store and print the most common VP-chunks here
-
+most_common_vp_chunks = vp_chunk_counter(vp_chunked_text)
+print(most_common_vp_chunks)
